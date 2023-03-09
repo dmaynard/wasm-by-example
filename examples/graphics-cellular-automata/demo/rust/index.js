e9d198502c5af8393c524215e2e01cc63a08a614
dmaynard <davidsmaynard@gmail.com>
2023-03-08 21:14:58 -0800

    addded crystal graphics using 4*[u8]s is wasm rather than [u32]s

---
+++ examples/graphics-cellular-automata/demo/rust/index.js
@@ -0,0 +1,115 @@
+import wasmInit from "./pkg/cellular.js";
+
+const runWasm = async () => {
+  // Instantiate our wasm module
+  const rustWasm = await wasmInit("./pkg/cellular_bg.wasm");
+
+  // Get our canvas element from our index.html
+  const canvasElement = document.querySelector("canvas");
+
+  // Set up Context and ImageData on the canvas
+  const canvasContext = canvasElement.getContext("2d");
+  const canvasImageData = canvasContext.createImageData(
+    canvasElement.width,
+    canvasElement.height
+  );
+
+  // Clear the canvas
+  canvasContext.clearRect(0, 0, canvasElement.width, canvasElement.height);
+  // We want to stop the reaction when either no pixels change,
+  // or the pattern stablizes.
+  let restart = false;
+  let last_ndelta = 0;
+  let streak = 0;
+  let paused = false;
+  let delayms = 128;
+  let logPerfArraySize = 6;
+  let framePerfs = new Array(2 ** logPerfArraySize);
+  let frame = 1;
+  let color = true;
+
+  document.addEventListener(
+    "keyup",
+    event => {
+      var name = event.key;
+      var code = event.code;
+      if (event.code == "Space") paused = !paused;
+      if (event.key == "r") restart = true;
+      if (event.key == "c") color = !color;
+      if (paused && event.key == ".") drawCrystal(false);
+      if (!paused && event.key == "f") {
+        delayms = delayms >= 8 ? delayms / 2 : delayms;
+        clearInterval(interval);
+        interval = setInterval(() => {
+          if (!paused) drawCrystal(restart);
+        }, delayms);
+      }
+
+      if (!paused && event.key == "s") {
+        delayms = delayms <= 2048 ? delayms * 2 : delayms;
+        clearInterval(interval);
+        interval = setInterval(() => {
+          if (!paused) drawCrystal(restart);
+        }, delayms);
+      }
+      // console.log(" key: %s code %s", name, code, paused, delayms);
+    },
+    false
+  );
+
+  const drawCrystal = init => {
+    const crtstalSize = 200;
+    // Create a Uint8Array to give us access to Wasm Memory
+    const wasmByteMemoryArray = new Uint8Array(rustWasm.memory.buffer);
+    const outputPointer = rustWasm.get_output_buffer_pointer();
+    let start = performance.now();
+    // Create or iterate the crystal automata in wasm
+    let n_deltas = rustWasm.update_crystal(init, color);
+    let end = performance.now();
+
+    if (n_deltas == last_ndelta) {
+      streak += 1;
+    } else {
+      streak = 0;
+      last_ndelta = n_deltas;
+    }
+
+    restart = n_deltas == 0 || streak > 200;
+
+    // Pull out the RGBA values from Wasm memory
+    // Starting at the memory index of out output buffer (given by our pointer)
+    // 20 * 20 * 4 = checkboard max X * checkerboard max Y * number of pixel properties (R,G.B,A)
+    // const outputPointer = rustWasm.get_output_buffer_pointer();
+    const imageDataArray = wasmByteMemoryArray.slice(
+      outputPointer,
+      outputPointer + crtstalSize * crtstalSize * 4
+    );
+
+    // Set the values to the canvas image data
+    canvasImageData.data.set(imageDataArray);
+
+    // Clear the canvas
+    canvasContext.clearRect(0, 0, canvasElement.width, canvasElement.height);
+
+    // Place the new generated checkerboard onto the canvas
+    canvasContext.putImageData(canvasImageData, 0, 0);
+
+    let findex = frame & (2 ** logPerfArraySize - 1);
+    framePerfs[findex] = end - start;
+    if (findex == 0) {
+      document.getElementById("WASMMS").innerHTML = (
+        framePerfs.reduce((a, b) => a + b, 0) / framePerfs.length
+      ).toFixed(2);
+    }
+
+    frame += 1;
+  };
+
+  // Lastly, call our function to draw the crystal
+  // And run this ten times every second
+  drawCrystal(true);
+  let interval = setInterval(() => {
+    if (!paused) drawCrystal(restart);
+  }, delayms);
+};
+runWasm();
---
+++ examples/graphics-cellular-automata/demo/rust/src/lib.rs
@@ -0,0 +1,169 @@
+// The wasm-pack uses wasm-bindgen to build and generate JavaScript binding file.
+// Import the wasm-bindgen crate.
+use wasm_bindgen::prelude::*;
+
+// I had trouble getting the Rust Rand crate to work in WASM
+// so instead I will use the ES6 random number generator
+// the sy_sys crate provide rust access to many ES6 system calls.
+// js_sys::Math::random() is the only one used here.
+extern crate js_sys;
+
+// Define the size of our "GRID"
+const GRID_W: u32 = 200;
+const GRID_H: u32 = 200;
+const GRID_SIZE: u32 = GRID_W * GRID_H;
+
+/*
+ * 1. What is going on here?
+ * WE create a static mutable buffer.
+ * We will use for putting the output of our graphics,
+ * to pass the output to js.
+ * NOTE: global `static mut` means we will have "unsafe" code
+ * but for passing memory between js and wasm should be fine
+ * since this code is single threaded.
+ *
+ * 2. In this example we treat the OUTPUT_BUFFER as an array of
+ * 32 bit pixels [u32: GRID_SIZE] (0xAABBGGRR) and store each
+ * entire pixel with a single u32 store.
+ */
+const OUTPUT_BUFFER_SIZE: usize = (GRID_SIZE * 4) as usize;
+static mut OUTPUT_BUFFER: [u8; OUTPUT_BUFFER_SIZE as usize] = [0; OUTPUT_BUFFER_SIZE as usize];
+
+static mut OLD_STATE: [u8; GRID_SIZE as usize] = [0; GRID_SIZE as usize];
+static mut NEW_STATE: [u8; GRID_SIZE as usize] = [0; GRID_SIZE as usize];
+
+const NUM_STATES: u8 = 16;
+
+const PALETTE: [(u8, u8, u8, u8); NUM_STATES as usize] = [
+    // (R,G,B,A)
+    (0xff, 0xff, 0xff, 0xff),
+    (0xee, 0xee, 0xee, 0xff),
+    (0xdd, 0xdd, 0xdd, 0xff),
+    (0xcc, 0xcc, 0xcc, 0xff),
+    (0xbb, 0xbb, 0xbb, 0xff),
+    (0xaa, 0xaa, 0xaa, 0xff),
+    (0x99, 0x99, 0x99, 0xff),
+    (0x88, 0x88, 0x88, 0xff),
+    (0x77, 0x77, 0x77, 0xff),
+    (0x66, 0x66, 0x66, 0xff),
+    (0x55, 0x55, 0x55, 0xff),
+    (0x44, 0x44, 0x44, 0xff),
+    (0x33, 0x33, 0x33, 0xff),
+    (0x22, 0x22, 0x22, 0xff),
+    (0x11, 0x11, 0x11, 0xff),
+    (0x00, 0x00, 0x00, 0xff),
+];
+const MATERIAL_PALETTE: [(u8, u8, u8, u8); NUM_STATES as usize] = [
+    //(R,G,B,A)
+    (244, 67, 54, 255),
+    (232, 30, 99, 255),
+    (156, 39, 176, 255),
+    (103, 58, 183, 255),
+    (63, 81, 181, 255),
+    (33, 150, 243, 255),
+    (3, 169, 244, 255),
+    (0, 188, 212, 255),
+    (0, 150, 136, 255),
+    (76, 175, 80, 255),
+    (139, 195, 74, 255),
+    (205, 220, 57, 255),
+    (255, 235, 59, 255),
+    (255, 193, 7, 255),
+    (255, 152, 0, 255),
+    (255, 87, 34, 255),
+];
+
+// Function to return a pointer to our buffer
+// in wasm memory
+#[wasm_bindgen]
+pub fn get_output_buffer_pointer() -> *const u8 {
+    let pointer: *const u8;
+    unsafe {
+        pointer = OUTPUT_BUFFER.as_ptr();
+    }
+    return pointer;
+}
+fn get_idx(x: u32, y: u32) -> usize {
+    match (y * GRID_W + x).try_into() {
+        Ok(idx) => idx,
+        Err(why) => panic!("{:?}", why),
+    }
+}
+fn get_neighbors(x: u32, y: u32) -> [usize; 4] {
+    [
+        // left
+        get_idx(if x == 0 { GRID_W - 1 } else { (x - 1) % GRID_W }, y),
+        // right
+        get_idx((x + 1) % GRID_W, y),
+        // above
+        get_idx(x, if y == 0 { GRID_H - 1 } else { (y - 1) % GRID_H }),
+        // below
+        get_idx(x, (y + 1) % GRID_H),
+    ]
+}
+// Function to generate the next generation of our crystal, pixel by pixel
+#[wasm_bindgen]
+pub fn update_crystal(init: bool, color: bool) -> u32 {
+    // Since Linear memory is a 1 dimensional array, but we want a grid
+    // we will be doing 2d to 1d mapping
+    // https://softwareengineering.stackexchange.com/questions/212808/treating-a-1d-data-structure-as-2d-grid
+    let mut n_deltas: u32 = 4;
+
+    let the_palette = if color { &MATERIAL_PALETTE } else { &PALETTE };
+    if init {
+        for y in 0..GRID_H {
+            for x in 0..GRID_W {
+                let state: u8 = (js_sys::Math::random() * NUM_STATES as f64) as u8;
+                let idx: usize = ((y * GRID_W) + x) as usize;
+                unsafe {
+                    NEW_STATE[idx] = state;
+                    OLD_STATE[idx] = state;
+                }
+            }
+        }
+    }
+    // update the new state from the old
+    for y in 0..GRID_H {
+        for x in 0..GRID_W {
+            let idx: usize = ((y * GRID_W) + x) as usize;
+            let neighbors: [usize; 4] = get_neighbors(x, y);
+            unsafe {
+                let cur_cell = OLD_STATE[idx];
+                for n_idx in neighbors {
+                    // if any neighbor is one state higher it eats this cell
+                    if OLD_STATE[n_idx] == (cur_cell + 1) % NUM_STATES {
+                        NEW_STATE[idx] = OLD_STATE[n_idx];
+                        n_deltas = n_deltas + 1;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    // map from state to color
+    for y in 0..GRID_H {
+        for x in 0..GRID_W {
+            let idx: usize = get_idx(x, y);
+            let byte_idx = idx * 4;
+            unsafe {
+                OUTPUT_BUFFER[byte_idx] = the_palette[(NEW_STATE[idx]) as usize].0;
+                OUTPUT_BUFFER[byte_idx + 1] = the_palette[(NEW_STATE[idx]) as usize].1;
+                OUTPUT_BUFFER[byte_idx + 2] = the_palette[(NEW_STATE[idx]) as usize].2;
+                OUTPUT_BUFFER[byte_idx + 3] = the_palette[(NEW_STATE[idx]) as usize].3;
+            }
+        }
+    }
+    // new now becomes old
+    for y in 0..GRID_H {
+        for x in 0..GRID_W {
+            let idx: usize = get_idx(x, y);
+            unsafe {
+                OLD_STATE[idx] = NEW_STATE[idx];
+            }
+        }
+    }
+    // return the number of pixels updated
+    // the Javascript code uses this to determine when to
+    // start a new crystal
+    n_deltas
+}
